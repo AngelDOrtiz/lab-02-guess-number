9343001df7a5e1fc57b00339066f4ea20845f2d7
AngelDOrtiz <aortizghs@gmail.com>
2021-03-24 17:31:27 -0700

    added some more function to the button

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 // import functions and grab DOM elements
-import { compareNumbers } from './utils'
+import { compareNumbers } from './utils.js';
 const userGuess = document.getElementById('user-guess');
-const guessBUtton = document.getElementById('guess-button');
+const guessButton = document.getElementById('guess-button');
+const guesses = document.getElementById('guesses');
+const retryButton = document.getElementById('retry');
+
 
 
 // initialize state
- 
+let remainingGuesses = 3;
+let correctNumber = 0;
 // set event listeners to update state and DOM
-addEventListener.
+guessButton.addEventListener('click', () => {
+    let userGuesses = userGuess.value;
+    const results = compareNumbers(userGuesses, correctNumber);
+    
+    if (results < correctNumber) {
+        alert('Too Low!');
+        remainingGuesses--;
+    }
+
+    if (results > correctNumber) {
+        alert('Too High!');
+        remainingGuesses--;
+    }
+
+    if (results === correctNumber) {
+        alert('YOU WIN');
+        retryButton.classList.remove('retry');
+        retryButton.textContent = 'Play Again';
+    }
+
+    guesses.textContent = remainingGuesses;
+
+    if (remainingGuesses === 0) {
+        retryButton.classList.remove('retry');
+    }
+});
+
